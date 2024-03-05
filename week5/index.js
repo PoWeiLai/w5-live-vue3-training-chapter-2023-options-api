60cf3e25b53854f7ad493fc26fcb4c367b13372c
Po Wei <lapo090790@shms-mail.ch>
2024-03-05 22:45:02 +0800

    git init

--- week5/index.js
+++ week5/index.js
@@ -1,9 +1,18 @@
 
-
-const apiName="powei"
+    const apiName="powei"
 const apiUrl="https://vue3-course-api.hexschool.io/v2"
-
-const app=createApp({
+import { defineRule } from 'vee-validate';
+import { required, email, min } from '@vee-validate/rules';
+defineRule('required', required);
+defineRule('email', email);
+defineRule('min', min);
+defineRule('min', min);
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+VeeValidate.configure({
+    generateMessage: VeeValidateI18n.localize('zh_TW'),
+    validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+  });
+createApp({
 data(){
  return{
     products:[],
@@ -16,9 +25,18 @@ methods:{
         }).catch(error=>{
             console.log(error)
         })
+    },
+    sendTo(){
+        
     }
+},
+mounted(){
+    this.getProduct()
 }
 })
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
 app.mount("#app")
 
 
