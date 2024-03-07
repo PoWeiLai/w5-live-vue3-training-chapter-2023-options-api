cacc3eebb958178f7e85ed741b7e3b0966ce407d
Po Wei <lapo090790@shms-mail.ch>
2024-03-07 23:08:02 +0800

    FF

--- week5/index.js
+++ week5/index.js
@@ -7,36 +7,37 @@ defineRule('required', required);
 defineRule('email', email);
 defineRule('min', min);
 defineRule('min', min);
-VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
 VeeValidate.configure({
     generateMessage: VeeValidateI18n.localize('zh_TW'),
     validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
   });
-createApp({
-data(){
- return{
-    products:[],
- }
-},
-methods:{
-    getProduct(){
-        axios.get(`${apiUrl}/api/${apiName}/products`).then(res=>{
-       console.log(res)
-        }).catch(error=>{
-            console.log(error)
-        })
+  const app = Vue.createApp({
+    data() {
+      return {
+        products: [],
+      };
     },
-    sendTo(){
-        
-    }
-},
-mounted(){
-    this.getProduct()
-}
-})
-app.component('VForm', VeeValidate.Form);
-app.component('VField', VeeValidate.Field);
-app.component('ErrorMessage', VeeValidate.ErrorMessage);
+    methods: {
+      getProduct() {
+        axios
+          .get(`${apiUrl}/api/${apiName}/products`)
+          .then(res => {
+            console.log(res);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      },
+    },
+    mounted() {
+      this.getProduct();
+    },
+  });
+
+// app.component('VForm', VeeValidate.Form);
+// app.component('VField', VeeValidate.Field);
+// app.component('ErrorMessage', VeeValidate.ErrorMessage);
 app.mount("#app")
 
 
