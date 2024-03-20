e3c955e96bfdab248f013cfdaeaf96aab92fb146
Po Wei <lapo090790@shms-mail.ch>
2024-03-21 00:50:57 +0800

    write

--- week5/index.js
+++ week5/index.js
@@ -1,33 +1,56 @@
 
-    const apiName="powei"
+const apiName="powei"
 const apiUrl="https://vue3-course-api.hexschool.io/v2"
-import { defineRule } from 'vee-validate';
-import { required, email, min } from '@vee-validate/rules';
+const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
+const { required, email, min, max } = VeeValidateRules;
+const { localize, loadLocaleFromURL } = VeeValidateI18n;
+//1.驗證可以改成import ... from ，我看文件是這樣寫，而且還發現import ...from後面還需要單引號?
 defineRule('required', required);
 defineRule('email', email);
 defineRule('min', min);
-defineRule('min', min);
-VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+defineRule('max', max);
+// 讀取外部的資源
+VeeValidateI18n.loadLocaleFromURL('https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json');
+
+// Activate the locale
 VeeValidate.configure({
-    generateMessage: VeeValidateI18n.localize('zh_TW'),
-    validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
-  });
-createApp({
+  generateMessage: VeeValidateI18n.localize('zh_TW'),
+  validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+});
+const { createApp } = Vue
+
+const app=createApp({
 data(){
  return{
     products:[],
+    form:{
+        email:'',
+        name:'',
+        tel:'',
+        address:'',
+    },
+    message:''
  }
 },
+
 methods:{
     getProduct(){
         axios.get(`${apiUrl}/api/${apiName}/products`).then(res=>{
-       console.log(res)
+     this.products=res.data.products
         }).catch(error=>{
             console.log(error)
         })
     },
+    isPhone(value) {
+        const phoneNumber = /^(09)[0-9]{8}$/
+        return phoneNumber.test(value) ? true : '需要正確的電話號碼'
+      },
     sendTo(){
-        
+        axios.post(`${apiUrl}/api/${apiName}/order`,{data:this.form}).then(res=>{
+            console.log(res)
+        }).catch(error=>{
+            console.log(error)
+        })
     }
 },
 mounted(){
