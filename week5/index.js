bdabc699d038f9b17d0c6ae17245c86a2eb62125
Po Wei <lapo090790@shms-mail.ch>
2024-03-21 21:41:27 +0800

    fix

--- week5/index.js
+++ week5/index.js
@@ -1,23 +1,15 @@
 
 const apiName="powei"
 const apiUrl="https://vue3-course-api.hexschool.io/v2"
-<<<<<<< HEAD
 const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
 const { required, email, min, max } = VeeValidateRules;
 const { localize, loadLocaleFromURL } = VeeValidateI18n;
 //1.驗證可以改成import ... from ，我看文件是這樣寫，而且還發現import ...from後面還需要單引號?
-=======
-
-const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
-const { required, email, min, max } = VeeValidateRules;
-const { localize, loadLocaleFromURL } = VeeValidateI18n;
-
->>>>>>> f4d70686b4ccf3f347537cf655856f1ae4cd649f
 defineRule('required', required);
 defineRule('email', email);
 defineRule('min', min);
 defineRule('max', max);
-<<<<<<< HEAD
+
 // 讀取外部的資源
 VeeValidateI18n.loadLocaleFromURL('https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json');
 
@@ -71,56 +63,8 @@ app.component('VField', VeeValidate.Field);
 app.component('ErrorMessage', VeeValidate.ErrorMessage);
 
 
-loadLocaleFromURL('https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json');
-
-configure({
-  generateMessage: localize('zh_TW'),
-});
-  const {createApp}=Vue
-  const app = Vue.createApp({
-    data() {
-      return {
-        products: [],
-        form:{
-         email:"",
-         name:"",
-         tel:"",
-         address:"",
-        },
-        message:""
-      };
-    },
-    components: {
-      VForm: Form,
-      VField: Field,
-      ErrorMessage: ErrorMessage,
-    },
-    methods: {
-      getProduct() {
-        axios
-          .get(`${apiUrl}/api/${apiName}/products`)
-          .then(res => {
-            console.log(res);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      },
-      sendTo(){
-        const order=this.form
-    axios.post(`${apiUrl}/api/${apiName}/order`,{data:order}).then((res)=>{
-      console.log(res)
-    })
- 
-      }
-    },
-    mounted() {
-      this.getProduct();
-    },
-  });
 
 
->>>>>>> f4d70686b4ccf3f347537cf655856f1ae4cd649f
 app.mount("#app")
 
 
